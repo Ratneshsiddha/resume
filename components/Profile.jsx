aeff9b52ea02af42e01b67f05e7c9dab6bddd2e1
Ratneshsiddha <ratneshd16@global.org.in>
2022-10-14 23:56:16 +0530

    fix rel noreferrer in ancor tag

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -14,19 +14,26 @@ const Profile = () => {
         </div>
         <div className="flex justify-center space-x-10 p-3 text-gray-600 text-2xl">
           <a
+            rel="noreferrer"
             target="_blank"
             href="https://www.linkedin.com/in/ratnesh-siddha-b0266a21b/"
           >
             <BsLinkedin className=" hover:scale-90 transition-all" />
           </a>
           <a
+            rel="noreferrer"
             target="_blank"
             href="https://twitter.com/SiddhaRatnesh"
-          ><BsTwitter className=" hover:scale-90 transition-all" /></a>
+          >
+            <BsTwitter className=" hover:scale-90 transition-all" />
+          </a>
           <a
+            rel="noreferrer"
             target="_blank"
             href="https://www.instagram.com/ratneshsiddh/"
-          ><BsInstagram className=" hover:scale-90 transition-all" /></a>
+          >
+            <BsInstagram className=" hover:scale-90 transition-all" />
+          </a>
         </div>
         <div className="flex justify-center">
           <button className="bg-cyan-600 w-full md:w-[85%] p-2 md:rounded-sm mb-3 hover:scale-95 transition-all text-white text-xl">
